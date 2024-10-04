a215ce8b4a48cd8d535f2a9b71e301c21c034f63
Putri Mila <putrimiila.fx@gmail.com>
2024-10-04 22:10:02 +0800

    add

---
+++ public/js/modal-video.js
@@ -0,0 +1,17 @@
+let videoFrame = document.getElementById('videoFrame');
+const targetEl = document.getElementById('video-modal');
+
+const options = {
+    placement: 'bottom-right',
+    backdrop: 'dynamic',
+    backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+    closable: true,
+    onHide: () => {
+        videoFrame.removeAttribute('src'); 
+    },
+    onShow: () => {
+        videoFrame.src = 'https://www.youtube.com/embed/ZWZfwmObdvc';
+    },
+};
+
+const modal = new Modal(targetEl, options);
